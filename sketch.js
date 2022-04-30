406adfa16f2ee6e5bd6121f4297a98d86b925301
Bernardo10nunes <104532757+Bernardo10nunes@users.noreply.github.com>
2022-04-30 10:42:54 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var caixa;
+function setup() {
+  createCanvas(400,400);
+  caixa=createSprite(50,50,20,20);
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+    caixa.position.x+=5;
+    background("red")
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    caixa.position.x-=5;
+  }
+  if(keyIsDown(UP_ARROW)){
+    caixa.position.y-=5;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    caixa.position.y+=5;
+  }
+  
+drawSprites();
+}
+
+
+
+
